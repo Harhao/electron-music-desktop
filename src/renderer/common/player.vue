951d97df4d40bf626e17c7f10e46c821e2f1e37a
Harhao <huahaoworkspace@163.com>
2020-01-12 19:06:44 +0800

    add the player feature

--- src/renderer/common/player.vue
+++ src/renderer/common/player.vue
@@ -1,29 +1,171 @@
 <template>
   <div class="player">
-    <el-button @click="showDetail">show</el-button>
+    <div class="play-progress">
+      <div class="tool-bar" :style="{width:toolBarWidth}"></div>
+      <span class="dot"></span>
+    </div>
+    <div class="control-pane">
+      <div class="left-side-info tool-item">
+        <div class="album-picture" @click="showDetail">
+          <img :src="demo" />
+          <div class="detail-arrow">
+            <span class="el-icon-arrow-up icon"></span>
+            <span class="el-icon-arrow-up icon last-icon"></span>
+          </div>
+        </div>
+        <div class="song-info">
+          <div class="song-name">
+            <span class="name">逃亡</span>
+            <span class="author">-（吴青峰）</span>
+          </div>
+          <div class="song-operate">
+            <span class="el-icon-thumb"></span>
+            <span class="el-icon-download"></span>
+            <span class="el-icon-chat-dot-square"></span>
+          </div>
+        </div>
+      </div>
+      <div class="control-tools tool-item">
+        <span class="el-icon-refresh"></span>
+        <span class="el-icon-refresh"></span>
+      </div>
+      <div class="right-side-info tool-item"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import demo from "@/assets/images/demo.jpg";
 export default {
   name: "player",
+  data() {
+    return {
+      demo,
+      toolBarWidth: 0
+    };
+  },
   methods: {
     showDetail() {
-      this.$store.dispatch("song/set_detail_show",true)
+      this.$store.dispatch("song/set_detail_show", true);
     }
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
-@import '../assets/styles/variable.scss';
+@import "../assets/styles/variable.scss";
 .player {
-    position: absolute;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 80px;
+  bottom: 0;
+  z-index: 99;
+  @include right_side_color;
+  .play-progress {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
     width: 100%;
-    height: 60px;
-    bottom: 0;
-    z-index: 99;
-    border-top: 1px solid #eaeaea;
-    @include right_side_color;
+    height: 2px;
+    -webkit-app-region: no-drag;
+    cursor: pointer;
+    background-color: #dddddd;
+    margin-top: 10px;
+    .tool-bar {
+      height: 2px;
+      background-color: #17d28d;
+    }
+    .dot {
+      display: inline-block;
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      cursor: pointer;
+      -webkit-app-region: no-drag;
+      background-color: #17d28d;
+    }
+  }
+  .control-pane {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex: 1;
+    .tool-item {
+      flex: 1;
+      height: 100%;
+    }
+    .left-side-info {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      box-sizing: border-box;
+      padding-left: 45px;
+      & .album-picture {
+        position: relative;
+        width: 40px;
+        height: 40px;
+        & > img {
+          width: 100%;
+          height: 100%;
+          border-radius: 5px;
+        }
+        .detail-arrow {
+          display: none;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          cursor: pointer;
+          background-color: rgba($color: #000000, $alpha: 0.3);
+          .icon {
+            display: block;
+            color: #ffffff;
+          }
+          .last-icon {
+            position: absolute;
+            top: 5px;
+          }
+        }
+        &:hover .detail-arrow {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+      }
+      .song-info {
+        flex: 1;
+        box-sizing: border-box;
+        padding-left: 15px;
+        height: 40px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        .song-name {
+          width: 100%;
+          font-size: 12px;
+          & .name {
+            color: #ffffff;
+          }
+          .author {
+            color: #dddddd;
+          }
+        }
+        .song-operate {
+          display: flex;
+          flex-direction: row;
+          & > span {
+            color: #ffffff;
+            font-size: 18px;
+            margin-right: 15px;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- src/renderer/components/BroadCast/index.vue
+++ src/renderer/components/BroadCast/index.vue
@@ -2,7 +2,7 @@
 
 <script>
 export default {
-  name: "player:broadcast"
+  name: "broadcast"
 };
 </script>
 
