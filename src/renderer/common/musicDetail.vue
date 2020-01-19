5edef58bf929f94ffaa49dee4d8fdf4d3e0158a0
huahao <lvhuahao@ad61v1.com>
2020-01-19 15:06:31 +0800

    add the frequencyData

--- src/renderer/common/musicDetail.vue
+++ src/renderer/common/musicDetail.vue
@@ -26,7 +26,14 @@
       </div>
     </div>
     <div class="music-canvas">
-
+      <canvas id="canvas"></canvas>
+      <audio
+        id="audio"
+        controls
+        src="http://183.47.253.143/amobile.music.tc.qq.com/C4000017BpKU1Sb8CL.m4a?guid=3534427900&vkey=86CBF0CEFF3FAF2C2C699715B9BDBF45152DDD1BF5B1757EB07718BFC5374D4F05EE11F6844CE93046F85AB0B9B7D1694A7F33A7EF0349FB&uin=0&fromtag=66"
+        crossOrigin="anonymous"
+      ></audio>
+      <a href="javascript:;" id="play-btn" @click="play">PLAY</a>
     </div>
     <div class="control">
       <player :isShowBackground="false" :isShowPicture="false"></player>
@@ -44,7 +51,8 @@ export default {
   },
   data() {
     return {
-      demo
+      demo,
+      audio: null
     };
   },
   methods: {
@@ -53,6 +61,49 @@ export default {
     },
     closeDetail() {
       this.$store.dispatch("song/set_detail_show", false);
+    },
+    play() {
+      const btn = document.getElementById("play-btn");
+      const audio = document.getElementById("audio");
+      btn.style.display = "none";
+      audio.play();
+      this.onLoadAudio(audio);
+    },
+    onLoadAudio(audio) {
+      var context = new (window.AudioContext || window.webkitAudioContext)();
+      var analyser = context.createAnalyser();
+      analyser.fftSize = 1024;
+      var source = context.createMediaElementSource(audio);
+
+      source.connect(analyser);
+      analyser.connect(context.destination);
+
+      var bufferLength = analyser.frequencyBinCount;
+      var dataArray = new Uint8Array(bufferLength);
+
+      let canvas = document.getElementById("canvas");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      var ctx = canvas.getContext("2d");
+      var WIDTH = canvas.width;
+      var HEIGHT = canvas.height;
+      var barWidth = (WIDTH / bufferLength) * 1.5;
+      var barHeight;
+      function renderFrame() {
+        requestAnimationFrame(renderFrame);
+        analyser.getByteFrequencyData(dataArray);
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        for (var i = 0, x = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i];
+          var r = barHeight + 300 * (i / bufferLength);
+          var g = 250 * (i / bufferLength);
+          var b = 255;
+          ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+          ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+          x += barWidth + 2;
+        }
+      }
+      renderFrame();
     }
   },
   computed: {
@@ -137,9 +188,40 @@ export default {
     }
   }
   .music-canvas {
+    position: relative;
     width: 80%;
-    height: 60px;  
+    // height: 60px;
+    height: 120px;
     margin: 0 auto;
+    #canvas {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+    audio {
+      visibility: hidden;
+    }
+    #play-btn {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: #13091b;
+      background: #007a99;
+      display: block;
+      width: 150px;
+      height: 45px;
+      line-height: 45px;
+      font-size: 18px;
+      cursor: pointer;
+      border-radius: 4px;
+      letter-spacing: 0.1em;
+      z-index: 1;
+      text-align: center;
+      text-decoration: none;
+    }
   }
   .control {
     position: relative;
