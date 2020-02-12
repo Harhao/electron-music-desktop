44f10fcf353c37adf199bfc244593d8b8d9fe475
huahao <lvhuahao@ad61v1.com>
2020-02-12 18:57:07 +0800

    :lipstick: fix the history

--- src/renderer/components/History/index.vue
+++ src/renderer/components/History/index.vue
@@ -3,14 +3,27 @@
     <h3 class="title">播放历史</h3>
     <div class="table-data">
       <span class="total">共150首</span>
-      <el-table :data="tableData" class="table-list" :cell-style="cellStyle" :header-cell-style="headerStyle">
+      <el-table
+        :data="tableData"
+        class="table-list"
+        :cell-style="cellStyle"
+        :header-cell-style="headerStyle"
+      >
         <el-table-column label="歌曲" align="center" width="400px">
           <template slot-scope="scope">
             <song :showImg="false"></song>
           </template>
         </el-table-column>
-        <el-table-column prop="singer" label="歌手" align="center"></el-table-column>
-        <el-table-column prop="album" label="专辑" align="center"></el-table-column>
+        <el-table-column
+          prop="singer"
+          label="歌手"
+          align="center"
+        ></el-table-column>
+        <el-table-column
+          prop="album"
+          label="专辑"
+          align="center"
+        ></el-table-column>
       </el-table>
     </div>
   </div>
@@ -45,18 +58,22 @@ export default {
     };
   },
   methods: {
-    headerStyle({row, rowIndex }) {
-      const theme = document.getElementById('app').getAttribute('data-theme')
-      switch(theme) {
-        case 'black': return {background: 'rgb(30, 30, 32)'}
-        case 'grey': return {background: 'rgb(82, 82, 82)'}
+    headerStyle({ row, rowIndex }) {
+      const theme = document.getElementById("app").getAttribute("data-theme");
+      switch (theme) {
+        case "black":
+          return { background: "rgb(30, 30, 32)" };
+        case "grey":
+          return { background: "rgb(82, 82, 82)" };
       }
     },
-    cellStyle({row, column, rowIndex, columnIndex}) {
-      const theme = document.getElementById('app').getAttribute('data-theme')
-      switch(theme) {
-        case 'black': return {background: 'rgb(30, 30, 32)'}
-        case 'grey': return {background: 'rgb(82, 82, 82)'}
+    cellStyle({ row, column, rowIndex, columnIndex }) {
+      const theme = document.getElementById("app").getAttribute("data-theme");
+      switch (theme) {
+        case "black":
+          return { background: "rgb(30, 30, 32)" };
+        case "grey":
+          return { background: "rgb(82, 82, 82)" };
       }
     }
   }
@@ -64,22 +81,22 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import '~@/assets/styles/variable.scss';
+@import "~@/assets/styles/variable.scss";
 .history-wrapper {
   position: relative;
   width: 100%;
   height: auto;
+  font-family: "source-beauty";
   .title {
     position: fixed;
     color: #ffffff;
     width: 100%;
     font-size: 30px;
-    font-family: "source-beauty";
     z-index: 99;
-     @include right_side_color;
+    @include right_side_color;
   }
   .table-data {
-    width: 100%;
+    width: 90%;
     box-sizing: border-box;
     min-height: 500px;
     padding-top: 50px;
@@ -87,14 +104,26 @@ export default {
       display: inline-block;
       color: #ddd;
       margin-bottom: 20px;
-
     }
-    /deep/ .el-table__row>td{
+    /deep/ .el-table td {
+      padding: 0;
+    }
+    /deep/ tr.el-table__row:hover {
+      background-color: #ddd;
+      cursor: pointer;
+    }
+    /deep/ .el-table__row > td {
       border: none;
       color: #ffffff;
     }
     /deep/.el-table::before {
       height: 0px;
+    }
+
+    /deep/ .el-table th.is-leaf,
+    .el-table td {
+      border: none;
+      text-align: center;
     }
   }
 }
